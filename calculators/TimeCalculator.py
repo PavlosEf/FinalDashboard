84256639b1d622ce779bd610a2445f68154427ca
PavlosEf <pefthimiadis1@gmail.com>
2025-03-10 15:57:45 +0200

    Update TimeCalculator.py

--- calculators/TimeCalculator.py
+++ calculators/TimeCalculator.py
@@ -50,7 +50,7 @@ def run():
             flag_file = data["flag"]
             
             # Create columns for flag, country name, and time
-            col1, col2, col3 = st.columns([1, 3, 2])
+            col1, col2, col3 = st.columns([1, 1, 1])
             with col1:
                 flag = display_flag(flag_file)
                 if flag:
